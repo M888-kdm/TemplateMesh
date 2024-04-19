0fd92284660b1774c6866e062332ece18229ee42
Moussa Niang <niangm@ept.sn>
2024-04-19 08:55:05 +0000

    added assemble schema functionality

---
+++ assemble.ts
@@ -0,0 +1,38 @@
+const yaml = require('js-yaml');
+const fs = require('fs');
+const path = require('path');
+
+const datasources_dir = "datasources";
+const plugins_dir = "plugins";
+const mesh_config_file = ".meshrc";
+
+function loadConfiguration(configPath: string){
+    try {
+        return yaml.load(fs.readFileSync(configPath, 'utf8'));
+    } catch (e) {
+        return null;
+    }
+}
+
+function getAllConfigurations(dirPath: string): any {
+    const files = fs.readdirSync(dirPath); // Get filenames synchronously
+    const results =  files.map(file => loadConfiguration(path.join(dirPath, file)));
+    return results;
+}
+
+function writeConfiguration(config: any, configPath: string){
+    const yamlString = yaml.dump(config);
+    fs.writeFileSync(configPath, yamlString);
+}
+
+function assembleSchema(){
+    const datasources = getAllConfigurations(datasources_dir);
+    const plugins = getAllConfigurations(plugins_dir);
+
+    const meshConfig = loadConfiguration(mesh_config_file);
+
+    meshConfig.datasources = datasources;
+    meshConfig.plugins = plugins;
+
+    writeConfiguration(meshConfig, mesh_config_file);
+}
